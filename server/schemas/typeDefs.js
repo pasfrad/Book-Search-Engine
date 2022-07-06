b6298f4d3e644862607ffbabb3afc6d6116f4521
Reese <rd.hart@example.com>
2022-07-06 10:39:30 -0400

    finish typeDefs and remove user controller

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,48 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    bookCount: Int!
+    savedBooks: [Book]
+}
 
+type Book {
+    _id: ID!
+    authors: String
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+input bookInput {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password! String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(input: bookInput!): User
+    deleteBook(bookId: ID!): User
+}
 `;
 
 module.exports = typeDefs;
