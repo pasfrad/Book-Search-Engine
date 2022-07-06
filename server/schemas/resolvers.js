3eb8539b74d01125105ee3ec51f380e4082f78f0
Reese <rd.hart@example.com>
2022-07-06 11:09:48 -0400

    remove routes and write part of resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,42 @@
 const { User } = require('./models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
+    Query: {
+     me: async (parent, args, context) => {
+        if (context.user) {
+            return User.findOne({ _id: context.user._id }).populate('savedBooks');
+        }
+        throw new AuthenticationError('Log in to see your books')      
+    },
+},
 
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password}) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Something went wrong');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Something went wrong');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            
+        },
+        deleteBook: async (parent, args, context) => {
+            
+        },
+    }
 };
 
 module.exports = resolvers;
